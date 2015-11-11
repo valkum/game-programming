e5d73ac02fb7a22606c9a156974404e5cdd8071b
Valkum <rudi.floren@googlemail.com>
2015-11-11 18:05:28 +0100

    moved to cmake and glfw

---
+++ include/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+GLFWwindow* g_window;
+
+int createWindow();
+int startGame();
+int draw(float delta);
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,103 @@
-#include <QtWidgets>
-#include "mainwindow.h"
+#include <GLFW/glfw3.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
 
 int main(int argc, char *argv[])
 {
-
-    QApplication app(argc, argv);
-    MainWindow mainWin;
-    mainWin.show();
-
-    return app.exec();
+    startGame();
+    
 
 }
-/**
-function loopyLoop() {
-    const int TICKS_PER_SECOND = 64;
-    const int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
-    const int MAX_FRAMESKIP = 10;
+int createWindow() {
+/* Initialize the library */
+    if (!glfwInit())
+        return false;
 
-    DWORD next_game_tick = WallClock();
+    /* Create a windowed mode window and its OpenGL context */
+    g_window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!g_window)
+    {
+        glfwTerminate();
+        return false;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(g_window);
+    return true;
+}
+
+int startGame() {
+    if ( !createWindow() ) {
+        glfwTerminate();
+        exit( -1 );
+    }
+
+
+    const int TICKS_PER_SECOND = 64;
+    const float frameTime = 1 / TICKS_PER_SECOND;
+    const int MAX_FRAMESKIP = 5;
+
+    double newTime = glfwGetTime();
     int loops;
-    float interpolation;
+    float extrapolation;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(g_window))
+    {
+        loops = 0;
+        while( glfwGetTime() > newTime && loops < MAX_FRAMESKIP) {
+            /* Poll for and process events */
+            glfwPollEvents();
+            //update_game(frameTime);
+
+            newTime += frameTime;
+            loops++;
+        }
+
+        extrapolation = glfwGetTime() - (newTime - frameTime);
+        draw( extrapolation );
+
+        
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+int  draw(float delta) {
+    int width, height;
+    glfwGetFramebufferSize(g_window, &width, &height);
+    
+
+    glfwSwapBuffers(g_window);
+}
+
+/**
+function loopyLoopExtra() {
+    const int TICKS_PER_SECOND = 64;
+    const float frameTime = 1 / TICKS_PER_SECOND;
+    const int MAX_FRAMESKIP = 5;
+
+    double newTime = glfwGetTime();
+    int loops;
+    float extrpolation;
 
     bool game_is_running = true;
     while( game_is_running ) {
 
         loops = 0;
-        while( WallClock() > next_game_tick && loops < MAX_FRAMESKIP) {
-            update_game();
+        while( glfwGetTime() > newtime && loops < MAX_FRAMESKIP) {
+            update_game(frameTime);
 
-            next_game_tick += SKIP_TICKS;
+            newtime += frameTime;
             loops++;
         }
 
-        interpolation = float( GetTickCount() + SKIP_TICKS - next_game_tick ) / float( SKIP_TICKS );
-        display_game( interpolation );
+        extrapolation = glfwGetTime() - (newTime - frameTime);
+        draw( extrapolation );
 
         //view_position = position + (speed * interpolation)
     }
