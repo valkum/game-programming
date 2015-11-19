39bf27737e4b5e76b260e81ea92fe60e7c08e369
Rudi Floren <rudi.floren@gmail.com>
2015-11-20 00:16:17 +0100

    Added prebuild glfw3 for 12.04 LTS

--- travis_get_glfw3.sh
+++ travis_get_glfw3.sh
@@ -2,9 +2,8 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/glfw3/lib" ]; then
-  wget https://github.com/glfw/glfw/archive/3.1.1.tar.gz
-  tar -xzvf 3.1.1.tar.gz
-  cd glfw-3.1.1 && cmake -DCMAKE_INSTALL_PREFIX=$HOME/glfw3 -DBUILD_SHARED_LIBS=ON . && make && make install
+  wget https://s3.eu-central-1.amazonaws.com/travis-ci-glfw3/glfw-3.1.2.tar.gz
+  tar -xzvf glfw-3.1.2.tar.gz
 else
   echo "Using cached directory."
 fi
