462474f391c4033b42084c5bef86f794c25fe9f4
Rudi Floren <rudi.floren@gmail.com>
2015-11-19 22:04:28 +0100

    Updated .travis.yml

--- travis_get_glfw3.sh
+++ travis_get_glfw3.sh
@@ -3,8 +3,8 @@ set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/glfw3/lib" ]; then
   wget https://github.com/glfw/glfw/archive/3.1.2.tar.gz
-  tar -xzvf 3.1.2.tar.gz
-  cd glfw-3.1.2 && cmake -DCMAKE_INSTALL_PREFIX=$HOME/glfw3 -DBUILD_SHARED_LIBS=ON . && make && make install
+  tar -xzvf 3.1.1.tar.gz
+  cd glfw-3.1.1 && cmake -DCMAKE_INSTALL_PREFIX=$HOME/glfw3 -DBUILD_SHARED_LIBS=ON . && make && make install
 else
   echo "Using cached directory."
 fi
