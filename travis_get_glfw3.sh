f5ac413e83a10f50e7de7529ac5087f82daa5e8f
Rudi Floren <rudi.floren@gmail.com>
2015-11-19 22:05:42 +0100

    Made glfw3 version compatible with cmake version on travis-ci

--- travis_get_glfw3.sh
+++ travis_get_glfw3.sh
@@ -2,7 +2,7 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/glfw3/lib" ]; then
-  wget https://github.com/glfw/glfw/archive/3.1.2.tar.gz
+  wget https://github.com/glfw/glfw/archive/3.1.1.tar.gz
   tar -xzvf 3.1.1.tar.gz
   cd glfw-3.1.1 && cmake -DCMAKE_INSTALL_PREFIX=$HOME/glfw3 -DBUILD_SHARED_LIBS=ON . && make && make install
 else
