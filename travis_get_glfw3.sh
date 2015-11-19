8b2beaf2eacf0793d06403bf5a5ce1f21827bf4a
Rudi Floren <rudi.floren@gmail.com>
2015-11-19 22:01:31 +0100

    Updated .travis.yml

--- travis_get_glfw3.sh
+++ travis_get_glfw3.sh
@@ -4,7 +4,7 @@ set -e
 if [ ! -d "$HOME/glfw3/lib" ]; then
   wget https://github.com/glfw/glfw/archive/3.1.2.tar.gz
   tar -xzvf 3.1.2.tar.gz
-  cd glfw-3.1.2 && cmake -DCMAKE_INSTALL_PREFIX=$HOME/glfw3 -DBUILD_SHARED_LIBS . && make && make install
+  cd glfw-3.1.2 && cmake -DCMAKE_INSTALL_PREFIX=$HOME/glfw3 -DBUILD_SHARED_LIBS=ON . && make && make install
 else
   echo "Using cached directory."
 fi
