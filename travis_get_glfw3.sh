b24a9c90e8aa5d62d32312266ac75ef7d3b50208
Rudi Floren <rudi.floren@gmail.com>
2015-11-20 02:06:27 +0100

    nun aber

--- travis_get_glfw3.sh
+++ travis_get_glfw3.sh
@@ -5,11 +5,7 @@ if [ ! -d "$HOME/glfw3/lib" ]; then
   cd $HOME
   wget https://s3.eu-central-1.amazonaws.com/travis-ci-glfw3/glfw-3.1.2.tar.gz
   tar -xzvf glfw-3.1.2.tar.gz
-  cp $HOME/glfw3/include/ /usr/local/include
-  cp $HOME/glfw3/lib/ /usr/local/lib
 else
   echo "Using cached directory."
-  cp -r $HOME/glfw3/include/* /usr/include/
-  cp -r $HOME/glfw3/lib/* /usr/lib/
 fi
 
