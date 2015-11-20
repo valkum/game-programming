32285f531009fa3e248e2184bb9acf85d26c1088
Rudi Floren <rudi.floren@gmail.com>
2015-11-20 01:47:51 +0100

    Fuck travis and Ubuntu 12.04 LTS

--- travis_get_glfw3.sh
+++ travis_get_glfw3.sh
@@ -5,6 +5,11 @@ if [ ! -d "$HOME/glfw3/lib" ]; then
   cd $HOME
   wget https://s3.eu-central-1.amazonaws.com/travis-ci-glfw3/glfw-3.1.2.tar.gz
   tar -xzvf glfw-3.1.2.tar.gz
+  cp $HOME/glfw3/include/ /usr/local/include
+  cp $HOME/glfw3/lib/ /usr/local/lib
 else
   echo "Using cached directory."
+  cp $HOME/glfw3/include/ /usr/local/include
+  cp $HOME/glfw3/lib/ /usr/local/lib
 fi
+
