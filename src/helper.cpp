8ac1dcacf4dce132864a14ed00c61eafb15ba7c5
Sebastian <sebastian.bockholt@rwth-aachen.de>
2015-11-24 15:07:56 +0100

    fixed linux build, added #include <unistd.h> to helper.cpp

--- src/helper.cpp
+++ src/helper.cpp
@@ -1,16 +1,17 @@
 #include "helper.h"
 #include <limits.h>
 #include <libgen.h>
+#include <unistd.h>
 
 namespace helper {
-std::string getExePath() {
-    char exe_path[PATH_MAX] = {0};
-    uint32_t bufsize = sizeof(exe_path);
-    #ifdef __APPLE__
-    _NSGetExecutablePath(exe_path, &bufsize);
-    #else
-    readlink("/proc/self/exe", exe_path, bufsize);
-    #endif
-    return dirname(exe_path);
-}
+    std::string getExePath() {
+        char exe_path[PATH_MAX] = {0};
+        uint32_t bufsize = sizeof(exe_path);
+        #ifdef __APPLE__
+            _NSGetExecutablePath(exe_path, &bufsize);
+        #else
+            readlink("/proc/self/exe", exe_path, bufsize);
+        #endif
+        return dirname(exe_path);
+    }
 }
