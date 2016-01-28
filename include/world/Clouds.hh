07765d8a0a38fd8a86ccf4492357709de7f8197f
HarHarLinks <kim.brose@rwth-aachen.de>
2016-01-28 12:03:01 +0100

    merge Cloud and Clouds into single class

--- include/world/Clouds.hh
+++ include/world/Clouds.hh
@@ -1,7 +1,11 @@
 #ifndef CLOUDS_HH
 #define CLOUDS_HH
 
+#include "world/CloudParticle.hh"
 #include "world/Cloud.hh"
+
+#include <ACGL/OpenGL/Objects.hh>
+#include <ACGL/Types.hh>
 
 class Clouds{
 public:
@@ -9,16 +13,22 @@ public:
   ACGL::OpenGL::SharedVertexArrayObject getVao() { return vao; }
   ACGL::OpenGL::SharedArrayBuffer getAb() {return ab;}
   inline ACGL::OpenGL::SharedTexture2D getTexture() {
-    return clouds[0].getTexture();
+    return this->cloudTex;
   }
-  void render(ACGL::OpenGL::SharedShaderProgram shader, glm::mat4 *viewProjectionMatrix);
   void update(float dt, vec3 wind);
+  void render(ACGL::OpenGL::SharedShaderProgram shader, glm::mat4 *viewProjectionMatrix);
 protected:
 private:
-  std::vector<Cloud> clouds;
   ACGL::OpenGL::SharedVertexArrayObject vao;
   ACGL::OpenGL::SharedArrayBuffer ab;
-  void spawn(uint_t size = 100, float x = 0.0f, float width = 0.0f, float z = 0.0f, float length = 0.0f, float y = 2.0f, float height = 0.0f);
+  ACGL::OpenGL::SharedTexture2D cloudTex;
+  uint_t particleAmount;
+  uint_t lastUsedParticle = 0;
+  std::vector<CloudParticle> particles;
+  void spawnCloud(uint_t size = 100, float x = 0.0f, float width = 0.0f, float z = 0.0f, float length = 0.0f, float y = 2.0f, float height = 0.0f);
+  uint_t firstUnusedParticle();
+  void respawnParticle(CloudParticle &particle, vec3 pos, float randomOffset = 0.0f, glm::vec3 offset = vec3(0.0f), float alpha = 1.0f, float life = 10.0f, vec3 velocity = vec3(0));
+  void smooth();
 };
 
 #endif // CLOUD_HH
--- src/world/Clouds.cpp
+++ src/world/Clouds.cpp
@@ -1,6 +1,7 @@
 #include "world/Clouds.hh"
 
 #include <glm/gtc/random.hpp>
+#include <ACGL/OpenGL/Creator/Texture2DCreator.hh>
 #include <ACGL/OpenGL/Managers.hh>
 
 using namespace ACGL::OpenGL;
@@ -18,7 +19,7 @@ static GLfloat particle_quad[6*5] = {
       -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 
   }; 
 
-Clouds::Clouds(uint_t amount, uint_t size, int width, int length) {
+Clouds::Clouds (uint_t amount, uint_t cloudSize, int width, int length) : particleAmount(amount*cloudSize) {
   ab = SharedArrayBuffer(new ArrayBuffer());
   ab->defineAttribute("aPosition", GL_FLOAT, 3);
   ab->defineAttribute("aTexCoord", GL_FLOAT, 2);
@@ -27,30 +28,141 @@ Clouds::Clouds(uint_t amount, uint_t size, int width, int length) {
   vao = SharedVertexArrayObject(new VertexArrayObject());
   vao->attachAllAttributes(ab);
 
+  cloudTex = ACGL::OpenGL::Texture2DFileManager::the()->get(ACGL::OpenGL::Texture2DCreator("cloud_particle.png"));
+
+  //alloc particles
+  for (uint_t i = 0; i < this->particleAmount; ++i) {
+    particles.push_back(CloudParticle());
+  }
+
   float half = width/2.0f;
 	for (uint_t i = 0; i < amount; ++i)
 	{
-		spawn(size, -half, (float)width, 0.0f, (float)length);
+		spawnCloud(cloudSize, -half, (float)width, 0.0f, (float)length);
 	}
 }
 
-void Clouds::spawn(uint_t size, float x, float width, float z, float length, float y, float height) {
+void Clouds::spawnCloud(uint_t size, float x, float width, float z, float length, float y, float height) {
+		//determine cloud center
+		//todo: adjust with global wind vector and flying speed, so the cloud is at the position when char arrives
 		x = glm::linearRand(x, x+width);
 		y = glm::linearRand(y, y+height);
 		z = glm::linearRand(z, z+length);
-		clouds.push_back(Cloud(size, vec3(x, y, z)));
+		// clouds.push_back(Cloud(size, vec3(x, y, z)));
+
+		//spawn particles around center
+		vec3 position = vec3(x, y, z);
+		vec3 lastPos = position;
+  	for (uint_t i = 0; i < size; ++i) {
+    	CloudParticle &p = particles[firstUnusedParticle()];
+   		respawnParticle(p, lastPos, .5f, vec3(0), 1.0f, 1.0f/*linearRand(10.0f, 15.0f)*/);
+   		lastPos = p.Position;
+   		if (abs(glm::distance(position, lastPos)) > 3.0f || distance(position.y, lastPos.y) > .4f) lastPos = position;
+  	}
+  	smooth();
 }
 
-void Clouds::render(ACGL::OpenGL::SharedShaderProgram shader, glm::mat4 *viewProjectionMatrix) {
-	for (Cloud cloud : clouds)
-	{
-		cloud.render(shader, viewProjectionMatrix, vao);
-	}
+uint_t Clouds::firstUnusedParticle() {
+    // First search from last used particle, this will usually return almost instantly
+    for (uint_t i = lastUsedParticle; i < particleAmount; ++i){
+        if (particles[i].Life <= 0.0f){
+            lastUsedParticle = i;
+            return i;
+        }
+    }
+    // Otherwise, do a linear search
+    for (uint_t i = 0; i < lastUsedParticle; ++i){
+        if (particles[i].Life <= 0.0f){
+            lastUsedParticle = i;
+            return i;
+        }
+    }
+    // All particles are taken, override the first one (note that if it repeatedly hits this case, more particles should be reserved)
+    debug()<<"All particles used"<<std::endl;
+    lastUsedParticle = 0;
+    return 0;
+}
+
+void Clouds::respawnParticle(CloudParticle &particle, vec3 pos, float randomOffset, glm::vec3 offset, float alpha, float life, vec3 velocity) {
+    glm::vec3 random = sphericalRand(randomOffset);
+    float rColor = 0.4 + ((rand() % 100) / 100.0f);
+    particle.Position = pos + random + offset;
+    particle.Color = glm::vec4(rColor, rColor, rColor, alpha);
+    particle.Life = life;
+    particle.Scale = glm::scale(vec3(particle.Life));
+    particle.Velocity = velocity;
+}
+
+void Clouds::smooth() {
+  float distance = 0.0f;
+  float neighbors;
+  // Update all particles
+  for (uint_t i = 0; i < this->particleAmount; ++i)
+  {
+      CloudParticle &p = this->particles[i];
+      if (p.Life > 0.0f) // particle is alive, thus update
+      {
+          neighbors = 0;
+          //todo: optimize usingsearchgrid
+          for (uint_t j = 0; j < this->particleAmount; ++j) // calculate liveness based on density of neighboring particles
+          {
+              CloudParticle &n = this->particles[j];
+              if (n.Life > 0.0f) // online live neighbors are relevant
+              {
+                  distance = glm::distance(n.Position, p.Position);
+                  if (distance > 0 && distance <= 0.75f){
+                      neighbors += 1/distance;
+                  }
+              }
+          }
+          float density = neighbors / 10.0f;
+          // debug() << "before: " << p.Life << endl;
+          if (density < 0.1f) { // Life in range 0.5 to 1.0
+              density = 0.1f;
+          } else if (p.Life > 1.0f) {
+              density = 1.0f;
+          }
+          // debug() << "after: " << p.Life << endl;
+          // p.Color.a -= tanh(p.Life);
+          p.Color.a *= 0.25f * density;
+      }
+  }
 }
 
 void Clouds::update(float dt, vec3 wind){
-	for (auto&& cloud : clouds)
-	{
-		cloud.update(dt, wind);
+	//sort() //include life?
+	//die()
+  for (CloudParticle particle : particles)
+  {
+    if (particle.Life > 0.0f)
+    {
+			particle.Velocity = wind; //add global wind flow
+			//collision (char & environment)
+			//viscosity
+			//update pos
+			particle.Position += particle.Velocity * dt;
+		}
 	}
+}
+
+void Clouds::render(ACGL::OpenGL::SharedShaderProgram shader, glm::mat4 *viewProjectionMatrix) {
+  // Use additive blending to give it a 'glow' effect
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+  shader->setUniform("uMVP", (*viewProjectionMatrix));
+  //todo sort for depth-test
+
+  for (CloudParticle particle : particles)
+  {
+      if (particle.Life > 0.0f)
+      {
+          shader->setUniform("uOffset", particle.Position);
+          shader->setUniform("uScale", particle.Scale);
+          shader->setUniform("uColor", particle.Color);
+          shader->setTexture("uTexture", cloudTex, 3);
+          
+          vao->render();
+      }
+  }
+  // Don't forget to reset to default blending mode
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
