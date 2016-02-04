b7716eb6b46c4a33a484cb60fe51ec275f13fa72
Valkum <rudi.floren@googlemail.com>
2016-02-04 01:26:34 +0100

    Fix phyiscs of cloth.
    The gravitiy force is applied each 1/64 second. Don't mulitpling it by 1/64 will cause the 46th time of the earth gravity applied to the cloth.

--- src/world/Character.cpp
+++ src/world/Character.cpp
@@ -78,7 +78,7 @@ void Character::rotateZ(float angle){
 }
 
 void Character::update(float dt){
-  cloth->addForce(gravity); // add gravity each frame, pointing down
+  cloth->addForce(gravity*dt); // add gravity each frame, pointing down
   //cloth->windForce(vec3(0.0f, 0.05f,0.2f));
   cloth->timeStep(dt); // calculate the particle positions of the next frame
   cloth->modelCollision();
