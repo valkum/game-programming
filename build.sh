17be82b7d2763eb635f3181cbb68ef17c719309d
Valkum <rudi.floren@gmail.com>
2016-02-05 17:45:31 +0100

    Fix build in Lab

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p build/Debug;
 cd build/Debug;
-cmake ../../ -DCMAKE_BUILD_TYPE=Debug;
-make -j 4;
+cmake -DCMAKE_BUILD_TYPE=Debug -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF ../../;
+make -j 8;
