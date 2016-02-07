b60e392cefeff5a204fdbf9cf3ebf5e404736d41
Valkum <rudi.floren@googlemail.com>
2016-02-07 21:30:28 +0100

    Increase FPS by using only one VBO per geometry, and use instances of that when render.

--- src/Level.cpp
+++ src/Level.cpp
@@ -1,6 +1,8 @@
 #include "Level.hh"
 
 #include <ACGL/Utils/Log.hh>
+#include <unordered_map>
+
 
 using namespace ACGL::Base;
 using namespace ACGL::Utils;
@@ -33,8 +35,20 @@ void Level::load(){
     skyDome = new SkyDome(Model("SkyDome.obj", levelJson["skydome"]["scale"]), levelJson["skydome"]["texture"]);
     terrain = new Terrain(levelJson["mapSize"].at(0), levelJson["mapSize"].at(1));
 
+    std::unordered_map<string, std::pair<ACGL::OpenGL::SharedVertexArrayObject, GLsizei>> geometries;
     for (auto object : levelJson["objects"]) {
-      levelObjects.push_back(new Object(Model(object["model"], object["scale"]), parseVec3(object["position"]), parseVec3(object["rotation"])));
+      auto search = geometries.find(object["model"]);
+      GLsizei primCount = 1;
+      if(search != geometries.end()){
+        search->second.second++;
+        primCount = search->second.second;
+        levelObjects.push_back(new Object(Model(search->second.first, object["scale"], primCount), parseVec3(object["position"]), parseVec3(object["rotation"])));
+      } else {
+        auto vao = ACGL::OpenGL::VertexArrayObjectCreator(object["model"].get<string>()).create();
+        geometries.insert(std::pair<string, std::pair<ACGL::OpenGL::SharedVertexArrayObject, GLsizei>>(object["model"].get<string>(), std::pair<ACGL::OpenGL::SharedVertexArrayObject, GLsizei>(vao, 1)));
+        levelObjects.push_back(new Object(Model(vao, object["scale"], primCount), parseVec3(object["position"]), parseVec3(object["rotation"])));
+      }
+      
     }
 
     clouds = new Clouds(levelJson["clouds"].at(0), levelJson["clouds"].at(1), levelJson["mapSize"].at(0), levelJson["mapSize"].at(1));
